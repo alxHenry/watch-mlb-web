e68b7861baf6011d0584248faa228e026c2c8b90
Alex H <5092131+alxHenry@users.noreply.github.com>
2020-07-26 13:37:31 -0700

    Only show one link for now

--- src/components/GameCard/StreamLinks.tsx
+++ src/components/GameCard/StreamLinks.tsx
@@ -7,16 +7,11 @@ interface StreamLinkProps {
 
 const textStyle = { fontWeight: "bold" } as React.CSSProperties;
 const linkProps = { target: "_blank", rel: "noreferrer noopener" };
-const spacerStyle = { marginRight: "4px" };
 
-const StreamLinks: FC<StreamLinkProps> = ({ homeStreamUrl, awayStreamUrl }) => (
+const StreamLinks: FC<StreamLinkProps> = ({ homeStreamUrl }) => (
   <div style={textStyle}>
-    <span>Stream: </span>
-    <a href={homeStreamUrl} style={spacerStyle} {...linkProps}>
-      Home
-    </a>
-    <a href={awayStreamUrl} {...linkProps}>
-      Away
+    <a href={homeStreamUrl} {...linkProps}>
+      Watch
     </a>
   </div>
 );
