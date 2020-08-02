0a9d1d24079e903602d16e840dd4fa1ba455568b
Alex H <5092131+alxHenry@users.noreply.github.com>
2020-08-01 19:16:17 -0700

    Add league logoes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,23 @@ import PageMLB from "./components/PageMLB";
 import { SportsLeague } from "./api/games/types";
 import PageNBA from "./components/PageNBA";
 
+const logoStyles = { marginRight: "8px", paddingBottom: "3px" };
+
+const mlbLogo = (
+  <img
+    src="https://www.bing.com/th?id=ALBUD5CE4B8FC8A1D6200445D98037887C4202AF3D6884CA2F5225FF17131AD7CA3D4&w=28&h=28"
+    alt="MLB"
+    style={logoStyles}
+  />
+);
+const nbaLogo = (
+  <img
+    src="https://www.bing.com/th?id=ALBUD70C4B1B94F8CEE97262F925B9AAA92DC8EDCAA8A5C92616268B595D9CD593AB3&w=28&h=28"
+    alt="NBA"
+    style={logoStyles}
+  />
+);
+
 const App: FC = () => {
   const [league, setLeague] = useState(SportsLeague.MLB);
   const handleMenuClick = useCallback((e: any) => {
@@ -25,8 +42,12 @@ const App: FC = () => {
   return (
     <>
       <Menu onClick={handleMenuClick} selectedKeys={[league]} mode="horizontal">
-        <Menu.Item key={SportsLeague.MLB}>MLB</Menu.Item>
-        <Menu.Item key={SportsLeague.NBA}>NBA</Menu.Item>
+        <Menu.Item key={SportsLeague.MLB} icon={mlbLogo}>
+          MLB
+        </Menu.Item>
+        <Menu.Item key={SportsLeague.NBA} icon={nbaLogo}>
+          NBA
+        </Menu.Item>
       </Menu>
       {page}
     </>
