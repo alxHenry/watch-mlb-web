33a679ea860b325b2e9dbbcc408e0aa25b9c553c
Alex H <5092131+alxHenry@users.noreply.github.com>
2020-08-01 17:20:21 -0700

    Add switcher for the different leagues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import "antd/dist/antd.css";
 
-import React, { FC } from "react";
+import React, { FC, useState, useCallback } from "react";
+import { Menu } from "antd";
 import PageMLB from "./components/PageMLB";
+import { SportsLeague } from "./api/games/types";
+import PageNBA from "./components/PageNBA";
 
 const App: FC = () => {
-  return <PageMLB />;
+  const [league, setLeague] = useState(SportsLeague.MLB);
+  const handleMenuClick = useCallback((e: any) => {
+    setLeague(e.key);
+  }, []);
+
+  let page;
+  switch (league) {
+    case SportsLeague.NBA:
+      page = <PageNBA />;
+      break;
+    case SportsLeague.MLB:
+    default:
+      page = <PageMLB />;
+  }
+
+  return (
+    <>
+      <Menu onClick={handleMenuClick} selectedKeys={[league]} mode="horizontal">
+        <Menu.Item key={SportsLeague.MLB}>MLB</Menu.Item>
+        <Menu.Item key={SportsLeague.NBA}>NBA</Menu.Item>
+      </Menu>
+      {page}
+    </>
+  );
 };
 
 export default App;
